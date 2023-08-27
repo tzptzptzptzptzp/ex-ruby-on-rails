00d5670ec73b7cfeafc7108c3b33c4cee6d71af3
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-27 23:35:06 +0900

    🔧 ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :update]
 end
